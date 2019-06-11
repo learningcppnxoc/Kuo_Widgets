f33c2d622409504319c034386811fe5322402eb9
RustledCrow <51672755+RustledCrow@users.noreply.github.com>
2019-06-11 13:44:52 -0700

    Initial commit
    Two examples that demonstrate basic concepts of C++ programming.

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int first_num;
+	int second_num;
+	
+	first_num = 29;
+	second_num = 30;
+	
+	cout << "Number first_num: " << first_num << endl;
+	cout << "Number second_num: " << second_num << endl;
+	
+	int sum_of_both;
+	sum_of_both = first_num + second_num;
+	
+	cout << "The sum of " << first_num << " and " << second_num << " is : " << sum_of_both << endl;
+	
+	return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Goodbye Kuo World!" << endl;
+	return 0;
+}
